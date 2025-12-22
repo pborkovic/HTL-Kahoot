637b1a63292152231f41409f119b471f57fc6d02
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-12-22 16:10:19 +0100

    Update `User` model to add new fillable attributes, update casts, and include relationship imports

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -15,23 +17,25 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'classifications',
+        'is_active',
+        'avatar',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
-        'remember_token',
-    ];
+        'classificationsArray'];
 
     /**
      * Get the attributes that should be cast.
@@ -43,6 +47,9 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'classifications' => 'array',
+            'is_active' => 'boolean',
         ];
     }
+
 }
