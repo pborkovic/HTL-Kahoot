d8ec317e4c97fc9d9e5ade6345a9711de33a2be5
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-01-19 15:09:52 +0100

    Enable soft deletes on `roles`, `permissions`, and `users` tables

--- backend/database/migrations/2026_01_19_133302_create_users_table.php
+++ backend/database/migrations/2026_01_19_133302_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('avatar_url')->nullable();
             $table->timestamp('last_login_at')->nullable();
             $table->timestamps();
+            $table->softDeletes();
 
             $table->index('email');
         });
