64d7b283f7b10f1b237618a4384441324ce10e66
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-01-19 15:05:19 +0100

    Add `fillable` attributes and `users` relationship to `Role` model

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            related: User::class
+        )->withTimestamps();
+    }
 }
