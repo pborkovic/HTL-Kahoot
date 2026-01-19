b2826f562692be20e59e878e37d24f3c0a3322da
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-01-19 15:59:19 +0100

    Add authentication routes to `api.php` with `AuthController`

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
         'message' => 'HTL Kahoot API',
     ]);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::get('redirect', [AuthController::class, 'redirect']);
+    Route::get('callback', [AuthController::class, 'callback']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('user', [AuthController::class, 'user']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
