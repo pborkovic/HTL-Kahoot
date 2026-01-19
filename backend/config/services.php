07c8db8c35b254a2a6d1f88139c61df1aaca16a3
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-01-19 16:08:31 +0100

    Add Azure configuration to services.php

--- backend/config/services.php
+++ backend/config/services.php
@@ -35,4 +35,11 @@ return [
         ],
     ],
 
+    'azure' => [
+        'client_id' => env('AZURE_CLIENT_ID'),
+        'client_secret' => env('AZURE_CLIENT_SECRET'),
+        'redirect' => env('AZURE_REDIRECT_URI'),
+        'tenant' => env('AZURE_TENANT_ID'),
+    ],
+
 ];
