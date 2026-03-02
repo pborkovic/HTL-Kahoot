40c7fae225da2ddce95936182828bbd79adb2a8f
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-03-02 13:11:02 +0100

    Add PHPDoc annotations for methods in `SessionService`

--- backend/app/Services/SessionService.php
+++ backend/app/Services/SessionService.php
@@ -18,6 +18,11 @@ class SessionService extends BaseService implements SessionServiceContract
         $this->repository = $repository;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @author Philipp Borkovic
+     */
     public function createGame(string $quizId, User $host): Session
     {
         $gamePin = $this->repository->generateUniqueGamePin();
@@ -35,6 +40,11 @@ class SessionService extends BaseService implements SessionServiceContract
         return $session->load(relations: ['quiz', 'host']);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @author Philipp Borkovic
+     */
     public function generateQrCodeDataUri(string $gamePin): string
     {
         $joinUrl = config('app.frontend_url', config('app.url')) . '/join/' . $gamePin;
