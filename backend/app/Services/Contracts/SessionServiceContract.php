47399308ec99a83ee766886accf9c96462960517
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-03-02 19:09:38 +0100

    Update `SessionServiceContract` to use `CreateSessionDto` in `createGame` method for consistency

--- backend/app/Services/Contracts/SessionServiceContract.php
+++ backend/app/Services/Contracts/SessionServiceContract.php
@@ -2,13 +2,14 @@
 
 namespace App\Services\Contracts;
 
+use App\DTOs\CreateSessionDto;
 use App\Models\Session;
 use App\Models\User;
 use App\Services\Base\Contracts\BaseServiceContract;
 
 interface SessionServiceContract extends BaseServiceContract
 {
-    public function createGame(string $quizId, User $host): Session;
+    public function createGame(CreateSessionDto $dto, User $host): Session;
 
     public function generateQrCodeDataUri(string $gamePin): string;
 }
