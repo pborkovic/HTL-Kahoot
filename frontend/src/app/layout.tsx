14aa5fec5f27202070a4b5a8a1266958afca590c
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-03-07 21:44:18 +0100

    Wrap `children` in `TooltipProvider` to enable tooltips

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AuthProvider } from "@/context/AuthContext";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import "./globals.css";
 
 const inter = Inter({
@@ -22,7 +23,9 @@ export default function RootLayout({children}: Readonly<{
         <html lang="de">
         <body className={`${inter.variable} antialiased`}>
         <AuthProvider>
-            {children}
+            <TooltipProvider>
+                {children}
+            </TooltipProvider>
         </AuthProvider>
         </body>
         </html>
