9f97885f3284f2dccd3e06769cee2fb4ee14bdb0
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-03-08 20:49:48 +0100

    Declare strict types, add exception handling and logging, and implement `deleteWithRelations` method in `PermissionService`

--- backend/app/Services/PermissionService.php
+++ backend/app/Services/PermissionService.php
@@ -1,11 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services;
 
 use App\Models\Permission;
 use App\Repositories\Contracts\PermissionRepositoryContract;
 use App\Services\Base\BaseService;
 use App\Services\Contracts\PermissionServiceContract;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class PermissionService extends BaseService implements PermissionServiceContract
 {
@@ -19,5 +23,26 @@ class PermissionService extends BaseService implements PermissionServiceContract
     public function getModelForPolicy(): string
     {
         return Permission::class;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @uthor Philipp Borkovic
+     */
+    public function deleteWithRelations(Permission $permission): void
+    {
+        try {
+            $permission->roles()->detach();
+            $this->repository->delete(id: $permission->id);
+        } catch (Exception $e) {
+            Log::error(message: "Service error deleting permission with relations: {$e->getMessage()}", context: [
+                'service'       => get_class($this),
+                'permission_id' => $permission->id,
+                'trace'         => $e->getTraceAsString(),
+            ]);
+
+            throw $e;
+        }
     }
 }
