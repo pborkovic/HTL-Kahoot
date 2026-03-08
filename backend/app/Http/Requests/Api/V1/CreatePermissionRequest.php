0cf120cf823da995ca7cf35807c11daec49d54c1
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-03-08 20:44:05 +0100

    Declare strict types and add type annotations in `CreatePermissionRequest`

--- backend/app/Http/Requests/Api/V1/CreatePermissionRequest.php
+++ backend/app/Http/Requests/Api/V1/CreatePermissionRequest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Requests\Api\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
@@ -11,6 +13,9 @@ class CreatePermissionRequest extends FormRequest
         return true;
     }
 
+    /**
+     * @return array<string, array<int, string>>
+     */
     public function rules(): array
     {
         return [
