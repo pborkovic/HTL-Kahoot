dde1ac8034db232fe3943abd87e239e0797f0c8c
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-03-08 20:48:42 +0100

    Declare strict types and add type annotations in `PermissionResource`

--- backend/app/Http/Resources/Api/V1/PermissionResource.php
+++ backend/app/Http/Resources/Api/V1/PermissionResource.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Resources\Api\V1;
 
 use Illuminate\Http\Request;
@@ -7,6 +9,9 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PermissionResource extends JsonResource
 {
+    /**
+     * @return array<string, mixed>
+     */
     public function toArray(Request $request): array
     {
         return [
