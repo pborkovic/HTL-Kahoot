0403bccc6e824dcaf8a6bd0b315889e334d58c05
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-03-11 10:05:00 +0100

    Add session management methods to `SessionRepository` for handling participants, questions, and responses

--- backend/app/Repositories/SessionRepository.php
+++ backend/app/Repositories/SessionRepository.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Models\Response;
 use App\Models\Session;
+use App\Models\SessionParticipant;
+use App\Models\SessionQuestion;
 use App\Repositories\Base\BaseRepository;
 use App\Repositories\Contracts\SessionRepositoryContract;
 use Exception;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class SessionRepository extends BaseRepository implements SessionRepositoryContract
@@ -97,5 +102,195 @@ class SessionRepository extends BaseRepository implements SessionRepositoryContr
         } while ($this->exists(field: 'game_pin', value: $pin));
 
         return $pin;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function updateSession(Session $session, array $data): void
+    {
+        $session->update(attributes: $data);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function refreshSession(Session $session): Session
+    {
+        return $session->fresh();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function loadSessionRelations(Session $session, string|array $relations): Session
+    {
+        return $session->load(relations: $relations);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function findParticipantByUserId(Session $session, string $userId): ?SessionParticipant
+    {
+        return $session->participants()
+            ->where(column: 'user_id', operator: '=', value: $userId)
+            ->first();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function createParticipant(Session $session, array $data): SessionParticipant
+    {
+        return $session->participants()->create(attributes: $data);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function countParticipants(Session $session): int
+    {
+        return $session->participants()->count();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function getParticipantsOrderedByScore(Session $session): Collection
+    {
+        return $session->participants()
+            ->orderByDesc(column: 'total_score')
+            ->get();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function incrementParticipantScore(SessionParticipant $participant, int $amount): void
+    {
+        $participant->increment(column: 'total_score', amount: $amount);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function createSessionQuestion(Session $session, array $data): SessionQuestion
+    {
+        return $session->sessionQuestions()->create(attributes: $data);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function countSessionQuestions(Session $session): int
+    {
+        return $session->sessionQuestions()->count();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function findSessionQuestionByDisplayOrder(Session $session, int $displayOrder): ?SessionQuestion
+    {
+        return $session->sessionQuestions()
+            ->where(column: 'display_order', operator: '=', value: $displayOrder)
+            ->first();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function findSessionQuestionByDisplayOrderOrFail(Session $session, int $displayOrder): SessionQuestion
+    {
+        return $session->sessionQuestions()
+            ->where(column: 'display_order', operator: '=', value: $displayOrder)
+            ->firstOrFail();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function updateSessionQuestion(SessionQuestion $sessionQuestion, array $data): void
+    {
+        $sessionQuestion->update(attributes: $data);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function loadSessionQuestionRelations(SessionQuestion $sessionQuestion, string|array $relations): SessionQuestion
+    {
+        return $sessionQuestion->load(relations: $relations);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function createResponse(SessionQuestion $sessionQuestion, array $data): Response
+    {
+        return $sessionQuestion->responses()->create(attributes: $data);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function countResponses(SessionQuestion $sessionQuestion): int
+    {
+        return $sessionQuestion->responses()->count();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function hasParticipantResponded(SessionQuestion $sessionQuestion, string $participantId): bool
+    {
+        return $sessionQuestion->responses()
+            ->where(column: 'participant_id', operator: '=', value: $participantId)
+            ->exists();
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @author Philipp Borkovic
+     */
+    public function wrapInTransaction(callable $callback): mixed
+    {
+        return DB::transaction(callback: $callback);
     }
 }
