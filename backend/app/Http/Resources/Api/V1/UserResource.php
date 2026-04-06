fe8a0bc5b39586820affb293ae62065e577940de
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-04-06 18:08:00 +0200

    Expose `preferences` field in `UserResource`.

--- backend/app/Http/Resources/Api/V1/UserResource.php
+++ backend/app/Http/Resources/Api/V1/UserResource.php
@@ -16,6 +16,7 @@ class UserResource extends JsonResource
             'display_name'  => $this->display_name,
             'avatar_url'    => $this->avatar_url,
             'class_name'    => $this->class_name,
+            'preferences'   => $this->preferences,
             'auth_provider' => $this->auth_provider,
             'is_active'     => $this->is_active,
             'totp_enabled'  => $this->totp_enabled,
